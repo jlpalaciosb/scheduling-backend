19ce15fcc150ce57f6c923bfc9b34159884dbe83
José Palacios <joselupalaciosbo@gmail.com>
2020-02-22 11:38:25 -0300

    create models

--- schedule/api/admin.py
+++ schedule/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from schedule.api.models import Service, Client, Professional, Appointment
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(Client)
+admin.site.register(Professional)
+admin.site.register(Appointment)
--- schedule/api/models.py
+++ schedule/api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150, null=True, blank=True)
+    category = models.CharField(max_length=50, null=True, blank=True)
+    professionals = models.ManyToManyField('api.Professional')
+
+
+class Person(models.Model):
+    id_document = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email_address = models.EmailField()
+    phone_number = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Client(Person):
+    pass
+
+
+class Professional(Person):
+    services = models.ManyToManyField(Service)
+    pass
+
+
+class Appointment(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+    finish_time = models.TimeField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    professional = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    state = models.CharField(max_length=1, choices=(('S', 'Scheduled'),
+            ('Y', 'Did attend'), ('N', 'Did not attend')))
+    comment = models.CharField(max_length=300, blank=True)
