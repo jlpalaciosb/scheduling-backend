d85fda72d2951e71900d478ac67c268a2cd3fa10
José Palacios <joselupalaciosbo@gmail.com>
2020-03-30 21:56:07 -0400

    client list filter search

--- schedule/api/views.py
+++ schedule/api/views.py
@@ -1,5 +1,5 @@
 from schedule.api.models import Service, Client, Professional, Appointment
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from schedule.api.serializers import ServiceSerializer, ClientSerializer, \
         ProfessionalSerializer, AppointmentSerializer
 from schedule.api.mixins import NotPatchMixin
@@ -18,16 +18,18 @@ class ClientViewSet(NotPatchMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows Clients to be viewed or edited.
     """
-    queryset = Client.objects.all().order_by('last_name')
+    queryset = Client.objects.all().order_by('last_name', 'first_name')
     serializer_class = ClientSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['full_name']
 
 
 class ProfessionalViewSet(NotPatchMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows Professionals to be viewed or edited.
     """
-    queryset = Professional.objects.all().order_by('last_name')
+    queryset = Professional.objects.all().order_by('last_name', 'first_name')
     serializer_class = ProfessionalSerializer
     permission_classes = [permissions.IsAuthenticated]
 
